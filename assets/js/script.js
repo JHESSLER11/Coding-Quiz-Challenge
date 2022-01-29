c14ab5f0784d30637e32a3cbec854ca5673a1d71
JHessler <jhessler11@gmail.com>
2022-01-29 12:07:21 -0500

    updated the framework

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+function startGame () {
+
+}
+
+
+
+const questions = [
+    {
+        question: 'How many chickens does it take to cross a road?',
+        answers: [ 
+            { text: 'five', correct: false },
+            { text: 'nine', correct: false},
+            { text: 'fourteen', correct: true},
+            { text: 'seven', correct: false}
+        ]
+    }
+]
