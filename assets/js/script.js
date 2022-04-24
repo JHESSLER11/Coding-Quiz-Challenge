b1cfc38be2939e9d976b19b16c486ffe43df03b0
JHessler <jhessler11@gmail.com>
2022-04-24 13:18:06 -0400

    rebuilding js file from scratch

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,111 +9,36 @@ let shuffledQuestions, currentQuestionIndex
 startButton.addEventListener('click', startGame)
 
 
-function startGame () {
-    console.log('started')
-    firstPage.classList.add('hide')
-    //random question so they're not always asked in same order 
-    shuffledQuestions = questions.sort(() => Math.random() - .5)
-    currentQuestionIndex = 0
-    
-    questionContainerEl.classList.remove('hide')
-    
-    setQuestion()
-}
-
-function setQuestion () {
-    resetState()
-    showQuestion(shuffledQuestions[currentQuestionIndex])
-
-
-}
-
-//shows question and runs correct or false logic 
-function showQuestion(question) {
-    questionEl.innerText = question.question
-    question.answers.forEach(answers => {
-        const button = document.createElement('button')
-        button.innerText = answers.text
-        button.classList.add('btn')
-        if(answers.correct) {
-            button.dataset.correct = answers.correct
-        }
-        button.addEventListener('click', selectAnswer)
-        answerButtonsEl.appendChild(button)
-    });
-    
-}
-
-function resetState() {
-    while (answerButtonsEl.firstChild) {
-        answerButtonsEl.removeChild(answerButtonsEl.firstChild)
-    }
-    // button.addEventListener('click', () => {
-    //     currentQuestionIndex++
-    //     setNextQuestion()
-    // })
-
-}
-
-
-function selectAnswer(e) {
-    const selectedButton = e.target
-    const correct = selectedButton.dataset.correct
-    setStatusClass(document.body, correct)
-    Array.from(answerButtonsEl.children).forEach(button => {
-        setStatusClass(button, button.dataset.correct)
-        
-    });
-    if (shuffledQuestions.length > currentQuestionIndex + 1) {
-
-
-    }
-}
-
-function setStatusClass(element, correct) {
-    clearStatusClass(element)
-    if (correct) {
-        element.classList.add('correct')
-    } 
-    else {
-        element.classList.add('correct')
-    }
-}
-
-function clearStatusClass(element) {
-    element.classList.remove('correct')
-    element.classList.remove('wrong')
-}
-
-
 const questions = [
+
     {
         question: 'How many chickens does it take to cross a road?',
-        answers: [ 
-            { text: 'five', correct: false },
-            { text: 'nine', correct: false},
-            { text: 'fourteen', correct: true},
-            { text: 'seven', correct: false}
-        ]
+        choices: ['one', 'two', 'three', 'four'],
+        answers: 'three'
     },
 
     {
-        question: 'How many xxxx does it take to cross a road?',
-        answers: [ 
-            { text: 'x', correct: false },
-            { text: 'x2', correct: false},
-            { text: 'fourteen', correct: true},
-            { text: 'seven', correct: false}
-        ]
+        question: 'How many chickens does it take to cross a road?',
+        choices: ['one', 'two', 'three', 'four'],
+        answers: 'three'
     },
 
     {
-        question: 'How many cens does it take to cross a road?',
-        answers: [ 
-            { text: 'five', correct: false },
-            { text: 'nine', correct: false},
-            { text: 'fourteen', correct: true},
-            { text: 'seven', correct: false}
-        ]
-    }
+        question: 'How many chickens does it take to cross a road?',
+        choices: ['one', 'two', 'three', 'four'],
+        answers: 'three'
+    },
+
+    {
+        question: 'How many chickens does it take to cross a road?',
+        choices: ['one', 'two', 'three', 'four'],
+        answers: 'three'
+    },
+
+    {
+        question: 'How many chickens does it take to cross a road?',
+        choices: ['one', 'two', 'three', 'four'],
+        answers: 'three'
+    },
+
 ]
